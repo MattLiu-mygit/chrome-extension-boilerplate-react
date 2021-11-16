8a6d5a0f167d3f6adc597e4fe4d69aae80e87f09
Matthew Liu <mliu78@jh.edu>
2021-11-16 15:16:26 -0500

    init commit

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,101 @@
-import { printLine } from './modules/print';
+import html2canvas from 'html2canvas';
+import resemble from 'resemblejs';
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+let snapshot = null;
+let blurred = false;
+let data = null;
 
-printLine("Using the 'printLine' function from the Print Module");
+function onBlur() {
+  blurred = true;
+}
+
+// When clicked on focus, this must only be after screen is blurred.
+function onFocus() {
+  const screenshotTarget = document.body;
+  if (blurred) {
+    html2canvas(screenshotTarget)
+      .then((canvas) => {
+        let current = canvas.toDataURL('image/png');
+        blurred = false;
+        getDiff(current);
+      })
+      .catch((err) => console.log('issue with html2canvas', err));
+  }
+}
+
+window.onfocus = onFocus;
+window.onblur = onBlur;
+
+// Takes a snapshot of the page every half second.
+const getSnapshot = () => {
+  if (!blurred) {
+    // Only take snapshots when not blurred
+    const screenshotTarget = document.body;
+    html2canvas(screenshotTarget)
+      .then((canvas) => {
+        snapshot = canvas.toDataURL('image/png');
+        setTimeout(() => getSnapshot(), 500);
+      })
+      .catch((err) => console.log('issue with html2canvas', err));
+  }
+};
+
+// Uses resemble to take a snapshot and then appends it to the document body.
+function getDiff(current) {
+  resemble.outputSettings({
+    errorColor: {
+      red: 255,
+      green: 0,
+      blue: 0,
+    },
+    errorType: 'movement',
+    transparency: 0,
+    largeImageThreshold: 1200,
+    useCrossOrigin: false,
+    outputDiff: true,
+  });
+  resemble(snapshot)
+    .compareTo(current)
+    .onComplete(async function (respData) {
+      const dataUrl = await respData.getImageDataUrl();
+      data = respData;
+
+      const img =
+        document.getElementById('change_overlay') === null
+          ? document.createElement('img')
+          : document.getElementById('change_overlay');
+      img.src = dataUrl;
+      img.style.position = 'absolute';
+      img.style.width = '100%';
+      img.style.top = 0;
+      img.style.left = 0;
+      img.id = 'change_overlay';
+      img.style.pointerEvents = 'none';
+      chrome.runtime.sendMessage({
+        message: 'data',
+        data: data.misMatchPercentage,
+      });
+      document.body.appendChild(img);
+
+      getSnapshot();
+    });
+}
+getSnapshot();
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === 'start') {
+    if (document.getElementById('change_overlay') !== null && data !== null) {
+      chrome.runtime.sendMessage({
+        message: 'data',
+        data: data.misMatchPercentage,
+      });
+    }
+  } else if (request.message === 'clear') {
+    data = null;
+    document.getElementById('change_overlay').src = null;
+    chrome.runtime.sendMessage({
+      message: 'data',
+      data: 0,
+    });
+  }
+});
---
+++ src/pages/Popup/Popup.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import './Popup.css';
+
+const Popup = () => {
+  const [misMatch, setMisMatch] = useState(0);
+  const [tabId, setTabId] = useState('');
+  const [history, setHistory] = useState([]);
+  useEffect(() => {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+      let activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {
+        message: 'start',
+        id: activeTab.id,
+      });
+      setTabId(activeTab.id);
+    });
+  }, []);
+
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
+    setHistory([...history, request.data]);
+    if (request.message === 'data') {
+      setMisMatch(request.data);
+    }
+  });
+
+  return (
+    <>
+      <div className="App" style={{ color: 'white' }}>
+        {history.map((item, index) => (
+          <h3>{item}</h3>
+        ))}
+        {misMatch > 0 ? (
+          <>
+            <h3>
+              Your website may have been maliciously changed while you were
+              somewhere else!
+            </h3>
+            <h3>You have a mismatch percentage of {misMatch}%!</h3>
+            <h3>
+              If you see any <h3 style={{ color: 'red' }}>red</h3> overlay on
+              the screen after clicking away, this content has changed!
+            </h3>
+            <button
+              style={{
+                padding: '1rem',
+                borderRadius: '0.25rem',
+                paddingTop: '0.5rem',
+                paddingBottom: '0.5rem',
+                backgroundColor: '#4CAF50',
+                color: 'white',
+              }}
+              onClick={() => {
+                chrome.tabs.sendMessage(tabId, { message: 'clear', id: tabId });
+              }}
+            >
+              Click here to clear overlay.
+            </button>
+          </>
+        ) : (
+          <>
+            <h3>No changes detected on your website since your last visit!</h3>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+export default Popup;
